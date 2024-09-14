461443b690581e5a7da9af7aeaaf15e54870ad84
DevionJones <devion.jones@hgs.hiddengeniusproject.org>
2024-09-14 11:51:58 -0700

    Update script.js

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,11 @@
 function counter() {
-// Write your code below. Hint: you can finish it in about 3-5 lines.
+    // Write your code below. Hint: you can finish it in about 3-5 lines.
+    // Variable = A storage Container for data
+    //Data Types: Integer,Float, String, Boolean
+
+    //Create a variable to store the tag we want to change
+        counter_number = document.getElementById('counter-number'); 
+
+        // Change the text of the tag we have stored in the variable above
+        counter_Number.innerHTML = parseInt(counter_number.innerHTML) + 1;
 }
