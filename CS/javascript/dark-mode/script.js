aebf8452bb4f6a6fb52125ae854431778f4b4cff
admin <adminadmin@Mysterious-OakBlk-device-to-be-locked1.local>
2024-10-12 12:54:56 -0700

    final-project

---
+++ CS/javascript/dark-mode/script.js
@@ -0,0 +1,4 @@
+function darkMode() {
+    document.body.style.backgroundColor = "black";
+    document.getElementsByTagName('h3').style.color = "white";
+}
--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,19 @@
-function modelPopUp() {
-    // Your code here.
-}   
+document.addEventListener("DOMContnetLoaded", () => {
+    // Get modal elements
+
+    // Event listner to open modal
+
+
+
+    //Event listner to close modal whern clicking on the 'X' button
+    closeModalButton.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+    //Event listner to close modal when clicking modal content
+    window.addEventListener("click", (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }    
+    });
+});
