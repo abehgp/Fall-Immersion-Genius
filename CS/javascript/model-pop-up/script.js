77353428dd368e99305e59c9948261ac40f7374a
admin <admin@oak12blk-Mac21.local>
2024-10-30 18:21:10 -0700

    finish-clothing-store-app

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,14 @@
-function modelPopUp() {
-    // Your code here.
-}   
+document.addEventListener("DOMContnetLoaded", () => {
+    // Get modal elements
+    // Event listner to open modal
+    //Event listner to close modal whern clicking on the 'X' button
+    closeModalButton.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+    //Event listner to close modal when clicking modal content
+    window.addEventListener("click", (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    });
+});
