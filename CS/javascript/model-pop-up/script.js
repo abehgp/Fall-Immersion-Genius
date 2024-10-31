bdf03bb5b2f4c39e2cc1fc8f342029aa64c6b102
neoshtml <xavier.lunarmstead@hgs.hiddengeniusproject.org>
2024-10-30 18:24:01 -0700

    pushing clothing store app

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,19 @@
-function modelPopUp() {
-    // Your code here.
-}   
+document.addEventListener("DOMContentLoaded", () => {
+    // Get modal elements
+
+
+    // Event listener to open modal
+
+
+    // Event listener to close modal when clicking on the 'X' button
+    closeModalButton.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+    // Event listener to close modal when clicking outside the modal content
+    window.addEventListener("click", (event) => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});
