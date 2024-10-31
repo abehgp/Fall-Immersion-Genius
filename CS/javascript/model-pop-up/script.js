aafeee6ade9a8d6fe79a1440b58786f304d9cb17
admin <admin@The-Hidden-Genius-Project.local>
2024-10-30 18:22:05 -0700

    finish-clothing-store-app

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,24 @@
-function modelPopUp() {
-    // Your code here.
-}   
+document.addEventListener('DOMContentLoaded', () => {
+    // Get the modal element
+    const modal = document.getElementById("myModal");
+    const openModalButton = document.getElementById('openModal');
+    const closeModalButton = document.getElementById('closeModal');
+
+    //Event listener for the open modal button
+    openModalButton.addEventListener('click', () => {
+    document.getElementById('myModal').style.display = 'block';
+});
+
+
+    //Event listener to close modal when clicking on the 'X' button
+    closeModalButton.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    //Event listener to close modal when clicking outside the modal
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
