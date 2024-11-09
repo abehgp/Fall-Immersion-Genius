db5d0f408fdd9a837b1bf1ac7d1a8e8d712b4dbb
admin <admin@The-Hidden-Genius-Project.local>
2024-11-09 13:05:54 -0800

    updated final-project

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,27 @@
-function modelPopUp() {
-    // Your code here.
-}   
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    const modal = document.getElementById("myModal");
+    const  openModalButton = document.getElementById("openModal");
+    const closeModalButton = document.getElementById("closeModal");
+ 
+ 
+
+
+ 
+
+     openModelButton.addEventListener("click", () =>{
+        model.style.display = "block";
+    });
+
+    closeModelButton.addEventListener("click", () =>{
+        model.style.display = "none";
+    });
+ 
+ 
+    window.addEventListener("click", (event) => {
+        if (event.target == modal) {
+            model.style.display = "none";
+        }
+    });
+ });
